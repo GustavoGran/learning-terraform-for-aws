92d91a3d90c7dffa3c430c64956729d19d5ff91b
GustavoGran <gustavo.grans@gmail.com>
2021-10-17 23:23:32 -0300

    Novos aprendizados

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,9 +1,20 @@
 # Configure S3 resource
 
+variable "env" {
+    type = string
+    default = "dev"
+    sensitive = true
+    description = "Environment variable relating to infrastructure code. Can be prod, dev or stg"
+    validation {
+        condition = length(var.env) <= 4 && contains(["prod","stg","dev"], var.env)
+        error_message = "Invalid 'env' input: env variable must be one of the following: prod, stg, dev."
+    }
+}
+
 resource "aws_s3_bucket" "test_bucket_alias" {
-    bucket = "my-test-tf-bucket"
+    bucket = "my-test-tf-bucket-${var.env}"
     tags = {
         Name = "My test TF Bucket"
-        Environment = "Dev"
+        Environment = var.env
     }
 }
